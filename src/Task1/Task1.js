906d369ec994dd607344d55297e6f209d6f3c424
SukhoruchkinAndrey17 <sukhoruchkinandrey@mail.ru>
2025-03-02 20:02:59 +0300

    Task1

---
+++ src/Task1/Task1.js
@@ -0,0 +1,14 @@
+import '..MainMenu';
+
+function Task1() {
+  return (
+    <div className="TaskContent">
+      <h1>1</h1>
+      <h1>2</h1>
+      <h1>3</h1>
+      <h1>4</h1>
+    </div>
+  );
+}
+
+export default Task1;
