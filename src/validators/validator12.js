6f21eab13facdad13be90341baf37e6198fb3fe5
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2025-03-13 14:59:21 +0300

    check hello

--- src/validators/validator12.js
+++ src/validators/validator12.js
@@ -1,5 +1,9 @@
+let needCheckHello = true;
+
 export function validate(text) {
-  if (text.toLowerCase() === "привет") {
+  const hasError = needCheckHello && text.toLowerCase() === "привет";
+
+  if (hasError) {
     throw new Error(
       "Ты нашел ошибку, сделай так чтобы она не падала и можно было ввести слово 'Привет' в поле ввода",
     );
